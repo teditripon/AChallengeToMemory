e5fc6aa4709e7adeb4d36df5e382d739c68ab9e8
Teodor-Dorin Tripon <dorin.tripon@yahoo.com>
2019-10-25 23:04:56 +0300

    added scoring and pair flip counter fixed errors caused by color names

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -3,13 +3,13 @@ import { alert } from "tns-core-modules/ui/dialogs";
 import { ActivatedRoute } from "@angular/router";
 
 
-const BACKGROUND_COLOR = "BurlyWood";
+const BACKGROUND_COLOR = "#DEB887";
 const BACKGROUND_IMAGE = "linear-gradient(45deg, blue,red,aqua,yellow,violet,green)";
 const COLOR_NAMES = [
-    'Black', 'Navy', 'Green','Purple', 'Teal', 'DeepSkyBlue', 'MediumSpringGreen', 'DodgerBlue', 'ForestGreen', 'DarkSlateGray ',
-    'DarkSlateBlue', 'Indigo', 'DarkOliveGreen', 'SlateBlue', 'LawnGreen','Blue', 'Maroon', 'YellowGreen', 'Brown', 'IndianRed',
-    'Chocolate', 'LightGrey', 'GoldenRod', 'RoyalBlue', 'LightCyan', 'OldLace', 'Red', 'DarkCyan ', 'Fuchsia', 'Pink', 'Gold',
-    'Turquoise', 'Yellow', 'MintCream', 'FireBrick', 'CornflowerBlue'];
+    '#000000', '#000080', '#00FF00', '#800080', '#008080', '#00BFFF', '#00FA9A', '#1E90FF', '#228B22', '#2F4F4F',
+    '#483D8B', '#4B0082', '#556B2F', '#6A5ACD', '#7CFC00', '#0000FF', '#800000', '#9ACD32', '#A52A2A', '#CD5C5C',
+    '#D2691E', '#D3D3D3', '#DAA520', '#4169E1', '#E0FFFF', '#FDF5E6', '#FF0000', '#008B8B', '#FF00FF', '#FFC0CB',
+    '#FFD700', '#40E0D0', '#FFFF00', '#F5FFFA', '#B22222', '#6495ED'];
 
 @Component({
     selector: "game-board",
@@ -19,7 +19,7 @@ const COLOR_NAMES = [
 export class GameBoardComponent implements OnInit {
     public pairFlipsCounter = 0;
     public matchedCounter = 0;
-
+    public score = 0;
     public preventTap = false;
     public tiles = [];
 
@@ -97,6 +97,7 @@ export class GameBoardComponent implements OnInit {
             if (this.selectedTile) {
 
                 if (this.selectedTile.index === tile.index) return; // taped same card again
+                
                 this.pairFlipsCounter++;
                 this.preventTap = true;
                 setTimeout(() => {
@@ -107,13 +108,13 @@ export class GameBoardComponent implements OnInit {
 
                         this.selectedTile.hidden = true;
                         tile.hidden = true;
-
+                        this.score += 100;
                         this.matchedCounter++;
                         this.preventTap = false;
                         this.selectedTile = undefined;
                         if (this.matchedCounter * 2 === this.numberOfCardsInDeck) {
                             alert(this.alertOptions).then(() => {
-                                this.createTiles();
+                                this.startNewGame();
                             });
                         }
                     } else {
@@ -158,5 +159,14 @@ export class GameBoardComponent implements OnInit {
             colorSets.push(...colors);
         }
         return colorSets;
+    }
+
+    private startNewGame(){
+        this.score = 0;
+        this.pairFlipsCounter = 0;
+        this.matchedCounter = 0;
+        this.preventTap = false;
+
+        this.createTiles();
     }
 }
